63bc46e5b789e46ffb0029f32fffd31b4e608037
Ahmad Neirat <ahmadneirat@outlook.com>
2022-07-16 20:07:59 +0300

    add aggregator functions

---
+++ fds-backend/src/ml-model/doctor-feature.model.ts
@@ -0,0 +1,8 @@
+export class DoctorFeatures {
+    ThrCodeMax: string;
+    Avgnumber_of_act: string;
+    avgSumClaim: string;
+    max_Doctor_visit_peryear: string;
+    medAvg: string;
+    avgSubscriberDoctorCost: string;
+}
---
+++ fds-backend/src/ml-model/patient-feature.model.ts
@@ -0,0 +1,11 @@
+export class PatientFeatures {
+    ThrCodeMax: string;
+    AGE: string;
+    CITY: string;
+    maxSUBVisitsSameDoctor: string;
+    number_of_visit_per_year: string;
+    Avgnumber_of_act: string;
+    avgSumClaimPerYear: string;
+    avgSumClaimPerVisit: string;
+    avgTimeDiff: string;
+}
